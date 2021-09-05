83d30a35a4410e3a430b3438d2bce70b7ce8a317
picaro <dmail2004@gmail.com>
2021-09-06 00:23:41 +1000

    stuff update lol

--- commands/github.js
+++ commands/github.js
@@ -3,6 +3,6 @@ module.exports = {
     description: "this is a github repo command",
     category: "dev stuff",
     execute(message, args){
-        message.channel.send('GitHub Repository for DoMikus Pizza\nignore this unless youre a dev -picaro\n')
+        message.channel.send('GitHub Repository for DoMikus Pizza\nignore this unless youre a dev -picaro\nhttps://github.com/picaro777/DoMikuPizza')
     },
 };
