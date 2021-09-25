aec0f7b7916fbfd7ef99e30146bb13c4b081a43c
picaro <dmail2004@gmail.com>
2021-09-25 19:51:22 +1000

    updated commands and jcommands lists

--- commands/jcommands.js
+++ commands/jcommands.js
@@ -3,6 +3,6 @@ module.exports = {
     description: "sends list of joke commands",
     category: "funny",
     execute(message, args){
-        message.channel.send(' **__DoMikus Pizza joke commands__**\n```ini[\n-frog: sends a frog\n-borgar: sends borgar\n-coconut: shows a coconut\n-death: i hate noid\n-gegcoin: sends gegcoin youtube\n-makingpizza: how we make pizza\n-noid: i still hate noid\n-stork: shows a shoebill stork\n-traitor: mcdonalds miku is NOT canon\n-troll: do an epic troll\n-fruits: bot reacts with 3 fruits]``` ')
+        message.channel.send(' **__DoMikus Pizza joke commands__**\n```ini[\n-frog: sends a frog\n-borgar: sends borgar\n-coconut: shows a coconut\n-death: i hate noid\n-gegcoin: sends gegcoin youtube\n-makingpizza: how we make pizza\n-noid: i still hate noid\n-stork: shows a shoebill stork\n-traitor: mcdonalds miku is NOT canon\n-troll: do an epic troll\n-fruits: bot reacts with 3 fruits\n-sad: sends cool animals so you arent sad\n-mikuiloveyou: miku loves you]``` ')
     },
 };
