cb5f09ae18e818377ece75b38d36b48b90f14005
picaro <35718789+picaro777@users.noreply.github.com>
2021-09-30 10:42:31 +1000

    Update dbInit.js

--- dbInit.js
+++ dbInit.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('database', 'user name', 'password', {
 	host: 'localhost',
 	dialect: 'sqlite',
 	logging: false,
